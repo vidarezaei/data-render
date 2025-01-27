534f0f1e7f15e3562bc7a223653ac8aff42a76aa
vidarezaei <vida.rezaei21@gmail.com>
2025-01-27 22:56:21 +0330

    changed to class

--- script.js
+++ script.js
@@ -1,66 +1,45 @@
+class CategoryRenderer {
+  constructor(containerId) {
+      this.container = document.getElementById(containerId);
+  }
 
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => {
-    const cat =data;
-    const container=document.getElementById("container");
-    container.append(printCat(data));
-    
-    
-    
- 
-    
-    
-    
-    
-    function createNode(element,value){
-        let finalElement=document.createElement(element);
-        finalElement.textContent=value;
-        return finalElement;
-    }
-    
-    
-    
-    function printCat(cat){
-        let ul=document.createElement("ul");
-    if(cat && cat.length>0){
-     
-        cat.forEach(item =>{
-                let elementId=createNode("li",item.id);
-                let elementName=createNode("li",item.name);
-                ul.append(elementId,elementName);
-                if(item.subCategories && item.subCategories.length>0){
-                    let subcat=printCat(item.subCategories);
-                    let elementSubCat=document.createElement("li") ; 
-                    elementSubCat.append(subcat);
-                    ul.append(elementSubCat);
-                }
-            } );
-        
-    }
-    return ul;
-    }
+  showPrintCat() {
+      fetch('./data.json')
+          .then(response => response.json())
+          .then(data => {
+              this.container.append(this.printCat(data));
+          })
+          .catch(error => {
+              console.error('Error loading JSON:', error);
+          });
+  }
 
+  createNode(element, value) {
+      let finalElement = document.createElement(element);
+      finalElement.textContent = value;
+      return finalElement;
+  }
+
+  printCat(cat) {
+      let ul = document.createElement("ul");
+      if (cat && cat.length > 0) {
+          cat.forEach(item => {
+              let elementId = this.createNode("li", item.id);
+              let elementName = this.createNode("li", item.name);
+              ul.append(elementId, elementName);
+              
+              if (item.subCategories && item.subCategories.length > 0) {
+                  let subcat = this.printCat(item.subCategories);
+                  let elementSubCat = document.createElement("li");
+                  elementSubCat.append(subcat);
+                  ul.append(elementSubCat);
+              }
+          });
+      }
+      return ul;
+  }
+}
 
 
-
-
-
-
-
-
-
-
-
-  })
-  .catch(error => {
-    console.error('Error loading JSON:', error);
-  });
-
-
-
-
-
- 
-
-
+const categoryRenderer = new CategoryRenderer("container");
+categoryRenderer.showPrintCat();
